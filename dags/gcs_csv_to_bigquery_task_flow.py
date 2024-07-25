b1d199e03b684c42299c19884f475f84a6de31ec
kiady andrianantoandro <kiady.andrianantoandro@gmail.com>
2024-07-25 21:26:06 +0000

    Added a dag to push from preprocessed in csv format  to bigquery

---
+++ dags/gcs_csv_to_bigquery_task_flow.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from airflow import DAG, XComArg
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+from airflow.providers.google.cloud.operators.bigquery import  BigQueryInsertJobOperator
+from airflow.operators.python import PythonOperator
+import os
+import re
+
+BUCKET = os.getenv('BUCKET_NAME')
+
+DATASET_NAME  = os.getenv('BQ_DATASET_NAME')
+
+
+def gcs_objects_to_load(**kwargs):
+    ti = kwargs["ti"]
+    objects_list = ti.xcom_pull(task_ids='get_all_objects')
+    current_date = kwargs['ds'].replace('-', '')                                #The ds represents the execution date so the dag can be
+    pattern = re.compile(rf'preprocessed/.*/{current_date}-\d+\.csv$')
+    object_to_process = [url for url in objects_list if pattern.search(url)]
+
+    return object_to_process
+
+
+def create_query(**kwargs):
+    ti = kwargs["ti"]
+    files = ti.xcom_pull(task_ids='gcs_objects_to_process')
+    files = [f'gs://{BUCKET}/'+ file for file in files]
+    INSERT_ROWS_QUERY = (
+    f"""
+    LOAD DATA INTO {DATASET_NAME}.raw (title string, link string, summary string, authors string, published string, tags string, inserted_at string, file_name string)
+    FROM FILES (format = 'CSV', uris = {str(files)});
+    """
+    )
+    return INSERT_ROWS_QUERY
+
+
+with DAG(dag_id="gcs_csv_to_big_query", start_date=datetime(2024, 6, 24), catchup = True) as dag:
+
+
+
+    files = GCSListObjectsOperator(
+        task_id="get_all_objects",
+        bucket= BUCKET,
+        prefix='preprocessed/',
+    )
+
+    files_filtered = PythonOperator(           #This task filters the file names to only keep the one uploaded on the execution date
+    task_id='gcs_objects_to_process',
+    python_callable=gcs_objects_to_load,
+    )
+
+
+    create_query_task = PythonOperator(
+    task_id = 'generate_query',
+    python_callable = create_query
+    )
+
+    insert_query_job = BigQueryInsertJobOperator(
+    task_id="insert_query_job",
+    configuration={
+            "query": {
+                "query": "{{  ti.xcom_pull(task_ids='generate_query')}}",
+                "useLegacySql": False,
+                "priority": "BATCH",
+            }
+        }
+    )
+
+
+    files >> files_filtered >> create_query_task >> insert_query_job
