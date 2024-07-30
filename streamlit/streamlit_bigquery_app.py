eb59cb5f2acc5337156e040159cfdab8c617e73f
Thomas Cadet <thomas.cadet@gmail.com>
2024-07-30 23:36:44 +0200

    feat: Add date picker !

--- streamlit/streamlit_bigquery_app.py
+++ streamlit/streamlit_bigquery_app.py
@@ -4,11 +4,15 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
+import datetime
+
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 client = bigquery.Client(credentials=credentials)
+
+
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -20,9 +24,16 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT title FROM `stay-informed-429009.dev_stay_informed.silver` LIMIT 10 ")
+
+start_date = datetime.date(2024, 6, 24)
+end_date = datetime.date.today() - datetime.timedelta(days=2)
+
+date = st.date_input("From which date do want somes news ?", end_date, start_date, end_date)
+
+rows = run_query(f"SELECT title, link FROM `stay-informed-429009.dev_stay_informed.gold` WHERE date = '{date}' LIMIT 10 ")
 
 # Print results.
-st.write("Queried from our dwh:")
+
+st.write("Somes articles from that days")
 for row in rows:
-    st.write("✍️ " + row['title'])
+    st.markdown("✍️ " + f"[{row['title']}]({row['link']})")
