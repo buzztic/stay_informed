e753e9a86de506801295a49c25706b84861b1d2c
yohann bacquey <yohann.bacquey@gmail.com>
2024-07-31 13:21:32 +0000

    convert static to dynamic variable everywhere(hopefully)

--- dbt_project/models/silver/silver.sql
+++ dbt_project/models/silver/silver.sql
@@ -5,7 +5,7 @@ WITH ranked_data AS (
         *,
         row_number() OVER (PARTITION BY title ORDER BY inserted_at DESC) AS row_num
     FROM
-        {{ var('dataset') }}.raw_rssfeed
+        {{ source('dev_stay_informed', 'raw') }}
 )
 
 SELECT
